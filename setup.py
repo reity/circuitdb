ef20e6009092887c3ee2212956fc526fe8c58139
Andrei Lapets <a@lapets.io>
2020-09-30 12:25:45 -0400

    Fix outputs to be forward-compatible with circuit library.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="circuitdb",
-    version="0.1.0",
+    version="0.1.1",
     packages=["circuitdb",],
     install_requires=["logical",],
     license="MIT",
