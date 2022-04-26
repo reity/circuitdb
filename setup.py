0a671a74753fa47062953572d14192a55b82249d
Andrei Lapets <a@lapets.io>
2022-04-26 17:33:19 -0400

    Test using pytest; update README, package files, and GitHub Actions workflow.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 # The lines below can be parsed by `docs/conf.py`.
 name = "circuitdb"
-version = "1.1.0"
+version = "1.2.0"
 
 setup(
     name=name,
@@ -20,6 +20,4 @@ setup(
                 "that have low arity.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    test_suite="nose.collector",
-    tests_require=["nose"],
 )
